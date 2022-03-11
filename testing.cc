484af55bcd46f11eeeb01c942917f5dc5a9deeb1
Richie Cahill <richie@tmmworkshop.com>
2022-03-11 00:33:19 -0500

    did more preformanas testing

--- testing.cc
+++ testing.cc
@@ -22,20 +22,20 @@ const string output = "/dev/null";
 
 int main(int argc, char const *argv[]){
 
-  uint64_t total = 100, avr=0;
+  // uint64_t total = 1000, avr=0;
 
   // for (uint64_t i = 0; i < total; i++){
 	
-  uint64_t shift=dist65(rng);
+  uint64_t shift=dist257(rng);
   
   clock_t t0 = clock();
-  __m256i	temp = _mm256_lrs_mm256_helper(_mm256_set_epi64x(0, 0, 0, 1ull),shift);  
+  __m256i	temp = _mm256_lrs_mm256(_mm256_set_epi64x(0, 0, 0, 1ull),shift);  
 	clock_t t1 = clock();
-	cout << (t1-t0) << endl;
+	// cout << (t1-t0) << endl;
   
   // avr+=(t1-t0);
   // __m256i	temp = _mm256_lls_mm256(_mm256_set_epi64x(0, 0, 0, 1ull),dist257(rng));
-  }
+  // }
   // cout << avr << endl;
   // cout << avr/total << endl;
 	return 0;
